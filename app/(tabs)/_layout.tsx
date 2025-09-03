02d8e97648c9a5be17e0dcb6616051c50d564aa3
DESKTOP-BIS6GKU\Mitrixlance <francis_2595@hotmail.com>
2025-09-02 18:55:27 -0600

    Adding task navigator

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { View, Text } from 'react-native'
+import { Tabs } from 'expo-router'
+import {Ionicons} from "@expo/vector-icons"
+
+const TabsLayout = () => {
+  return (
+    <Tabs 
+        screenOptions={{
+            tabBarActiveTintColor: "red",
+            tabBarInactiveTintColor: "green",
+            tabBarStyle: {
+                backgroundColor: "blue",
+                borderTopWidth: 1,
+                borderTopColor: "yellow",
+                height: 90,
+                paddingBottom: 30,
+                paddingTop:10,
+            },
+            tabBarLabelStyle: {
+                fontSize: 12,
+                fontWeight: "600"
+            },
+            headerShown: false,
+        }}
+    >
+        <Tabs.Screen
+            name='index'
+            options={{
+                title:"Todos",
+                tabBarIcon:({color,size}) => ( <Ionicons name='flash-outline' size={size} color={color}/>)
+            }}
+        />
+
+        <Tabs.Screen
+            name='Settings'
+            options={{
+                title:"Settings",
+                tabBarIcon:({color,size}) => ( <Ionicons name='settings' size={size} color={color}/>)
+            }}
+        />
+      
+    </Tabs>
+  )
+}
+
+export default TabsLayout
